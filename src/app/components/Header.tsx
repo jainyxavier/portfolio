1f98670fe18aa7a8d630282b0ec909ebdee84370
Jainy Xavier <xavierjainy15@gmail.com>
2023-12-21 17:33:20 -0300

    feat: menu mobile

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,11 @@
+"use client"
 import Image from "next/image";
-import { FaGithubAlt } from "react-icons/fa";
+import { useState } from "react";
 
 export default function Header() {
+
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const [classAnimation, setClassAnimation] = useState(false)
 
     const menuItems = [
         {
@@ -23,25 +27,54 @@ export default function Header() {
     ]
 
     return (
-        <nav className="w-full max-w-[1200px] mx-auto p-5 flex justify-between items-center">
-            <div>
-                <Image
-                className="rounded-xl shadow-lg shadow-gray-500/40 hover:shadow-gray-500/80"
-                    src="/logo.png"
-                    width={40}
-                    height={40}
-                    alt="Logo"
-                />
-            </div>
+        <nav className="w-full p-[20px] relative">
+            <div className="w-[calc(100%-40px)] max-w-[1200px] flex justify-between items-center mx-auto">
+                <div>
+                    <Image
+                    className="rounded-[20px] shadow-lg shadow-gray-500/40 hover:shadow-gray-500/80"
+                        src="/logo.png"
+                        width={40}
+                        height={40}
+                        alt="Logo"
+                    />
+                </div>
 
-            <div className="w-full max-w-[400px]">
-                <ul className="flex justify-between gap-4">
-                    {menuItems.map((item, index) => {
-                        return (
-                            <li key={index} className="text-white text-base font-semibold py-1.5 px-4 cursor-pointer hover:bg-white hover:text-pink-500 hover:rounded-lg hover:shadow-lg hover:shadow-gray-500/80">{item.name}</li>
-                        )
-                    })}
-                </ul>
+                {/* desktop */}
+                <div className="w-full max-w-[400px] flex max-[768px]:hidden">
+                    <ul className="w-full flex justify-between gap-4">
+                        {menuItems.map((item, index) => {
+                            return (
+                                <li key={index} className="text-white text-base font-semibold px-[15px] py-[5px] cursor-pointer hover:bg-white hover:text-pink-500 hover:rounded-lg hover:shadow-lg hover:shadow-gray-500/80">{item.name}</li>
+                            )
+                        })}
+                    </ul>
+                </div>
+
+                {/* mobile */}
+                {!toggleMenu ? (
+                    <div className="max-[768px]:flex hidden flex-col gap-[5px]" role="button" onClick={() => setToggleMenu(!toggleMenu)}>
+                        <div className="w-[25px] h-1 bg-[#D529A6] rounded-lg"></div>
+                        <div className="w-[25px] h-1 bg-[#D529A6] rounded-lg"></div>
+                        <div className="w-[25px] h-1 bg-[#D529A6] rounded-lg"></div>
+                    </div>
+                ) : (
+                    <div className="max-[768px]:flex transition w-[20px] flex-col gap-1 relative z-10" role="button" onClick={() => setToggleMenu(!toggleMenu)}>
+                        <div className="w-[25px] h-1 bg-[#D529A6] rounded-lg absolute rotate-[45deg]"></div>
+                        <div className="w-[25px] h-1 bg-[#D529A6] rounded-lg absolute rotate-[135deg]"></div>
+                    </div>
+                )}
+
+                {toggleMenu && (
+                    <div className="w-full flex absolute top-0 left-0 pt-[80px] bg-slate-900">
+                        <ul className="w-full flex justify-between gap-[15px] flex-col items-center">
+                            {menuItems.map((item, index) => {
+                                return (
+                                    <li key={index} className="text-white text-base font-semibold py-1.5 px-4 cursor-pointer hover:bg-white hover:text-pink-500 hover:rounded-lg hover:shadow-lg hover:shadow-gray-500/80">{item.name}</li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                )}
             </div>
         </nav>
     )
