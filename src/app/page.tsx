05c603011bf351adcb27bdf500f19f5d75e46334
Jainy Xavier <xavierjainy15@gmail.com>
2024-01-09 14:39:03 -0300

    feat: creation of the project section and style and text adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Roboto } from 'next/font/google'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Image from 'next/image'
 import { FaGithub, FaLinkedin, FaInstagramSquare, FaPhoneAlt, FaHtml5, FaCss3Alt, FaSass, FaReact, FaWordpress } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
@@ -15,7 +16,7 @@ export default function Home() {
       <Header />
 
       {/* home */}
-      <section className='w-full py-[80px]'>
+      <section className='w-full pt-[120px] pb-[120px]'>
         <div className='w-[calc(100%-40px)] max-w-[1200px] mx-auto flex justify-between items-center'>
           <div className='w-full max-w-[600px]'>
             <p className='w-fit bg-gradient-to-r from-[#bd3899] via-teal-500 to-fuchsia-500 text-transparent bg-clip-text text-[18px]'>Hello There !</p>
@@ -30,19 +31,19 @@ export default function Home() {
             <div className='w-full max-w-[350px] flex justify-between gap-[40px]'>
               <ul className='w-full flex justify-between items-center gap-[10px]'>
                 <li className='hover:scale-125 transition-all flex items-center justify-center'>
-                  <a className='p-[10px] rounded-full bg-white/10 hover:bg-white/20' href="https://www.linkedin.com/in/jainy-xavier-0288a3215/" target='_blank'>
+                  <a className='p-[10px] rounded-full hover:shadow-lg shadow-gray-500/40 hover:shadow-gray-500/80 bg-white/10 hover:bg-white/20' href="https://www.linkedin.com/in/jainy-xavier-0288a3215/" target='_blank'>
                     <FaGithub fontSize={'30px'} color="#f472b6" />
                   </a>
                 </li>
 
                 <li className='hover:scale-125 hover:transition-all flex items-center justify-center'>
-                  <a className='p-[10px] rounded-full bg-white/10 hover:bg-white/20' href="https://github.com/jainyxavier" target='_blank'>
+                  <a className='p-[10px] rounded-full hover:shadow-lg shadow-gray-500/40 hover:shadow-gray-500/80 bg-white/10 hover:bg-white/20' href="https://github.com/jainyxavier" target='_blank'>
                     <FaLinkedin fontSize={'30px'} color="#f472b6" />
                   </a>
                 </li>
 
                 <li className='hover:scale-125 hover:transition-all flex items-center justify-center'>
-                  <a className='p-[10px] rounded-full bg-white/10 hover:bg-white/20' href="https://www.instagram.com/jainy.xavier/" target='_blank'>
+                  <a className='p-[10px] rounded-full hover:shadow-lg shadow-gray-500/40 hover:shadow-gray-500/80 bg-white/10 hover:bg-white/20' href="https://www.instagram.com/jainy.xavier/" target='_blank'>
                     <FaInstagramSquare fontSize={'30px'} color="#f472b6" />
                   </a>
                 </li>
@@ -68,7 +69,7 @@ export default function Home() {
       </section>
 
       {/* about */}
-      <section className='w-full pb-[80px]'>
+      <section className='w-full pb-[120px]'>
         <div className='w-[calc(100%-40px)] max-w-[1200px] mx-auto'>
           <h2 className='text-[30px] text-center text-white font-semibold mb-[60px]'>About</h2>
 
@@ -82,25 +83,43 @@ export default function Home() {
             />
 
             <div className='w-full max-w-[550px]'>
-              <p className='text-[18px] text-[#99A2A5] font-semibold'>I am skilled in all aspects of the design process , from user research and wireframing to prototyping and implementation. I am also proficient in a variety of front-end development technologies, using Modern frameworks.</p>
+              <p className='text-[18px] text-[#99A2A5] font-medium'>
+                Durante os últimos quatro anos, tenho explorado profundamente o vasto universo da tecnologia, concentrando-me no desenvolvimento frontend desde 2020. Em setembro de 2022, tive a oportunidade de aplicar meu conhecimento ao ingressar no mercado de trabalho, colaborando em projetos de outsourcing com uma equipe incrível. Neste momento, estou em busca de novas oportunidades para continuar minha evolução nessa área que me apaixona, ansiosa por adquirir e compartilhar ainda mais conhecimento.
+              </p>
 
               <div className='w-[400px] h-[2px] bg-[#99A2A5] my-[25px]'></div>
 
               <div className='flex gap-[15px]'>
                 <FaLocationDot fontSize={'20px'} color="#f472b6" />
-                <span className='text-[16px] text-[#99A2A5]'>Petrolândia-PE</span>
+                <a href="https://petrolandiaparallax.netlify.app/" className='text-[16px] text-[#99A2A5] hover:text-white transition-all' target='_blank'>
+                  Petrolândia-PE
+                </a>
               </div>
 
               <div className='flex gap-[15px] my-[15px]'>
                 <FaPhoneAlt fontSize={'20px'} color="#f472b6" />
-                <a href="https://wa.me/5587999251839?text=Olá! Tudo bem?" className='text-[16px] text-[#99A2A5]' target='_blank'>
+                <a href="https://wa.me/5587999251839?text=Olá! Tudo bem?" className='text-[16px] text-[#99A2A5] hover:text-white transition-all' target='_blank'>
                   +55 87 9 9925-1839
                 </a>
               </div>
 
               <div className='flex gap-[15px]'>
                 <MdEmail fontSize={'20px'} color="#f472b6" />
-                <a href="mailto:xavierjainy15@gmail.com" className='text-[16px] text-[#99A2A5]'>xavierjainy15@gmail.com</a>
+                <a href="mailto:xavierjainy15@gmail.com" className='text-[16px] text-[#99A2A5] hover:text-white transition-all'>xavierjainy15@gmail.com</a>
+              </div>
+
+              <div className='flex gap-[40px] mt-[25px]'>
+                <a href="#" target="_blank" className='w-[200px] bg-white text-[#D529A6] font-semibold rounded-lg flex items-center justify-center shadow-lg shadow-gray-500 hover:scale-105'>
+                  Visualizar CV
+                </a>
+
+                <Image
+                  src="/img/arrow.webp"
+                  width={60}
+                  height={60}
+                  className='object-none'
+                  alt="Seta"
+                />
               </div>
             </div>
           </div>
@@ -108,7 +127,7 @@ export default function Home() {
       </section>
 
       {/* skills */}
-      <section className='w-full pb-[80px]'>
+      <section className='w-full pb-[120px]'>
         <div className='w-[calc(100%-40px)] max-w-[1200px] mx-auto'>
           <h2 className='text-[30px] text-center text-white font-semibold mb-[60px]'>Skills</h2>
 
@@ -143,6 +162,15 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      {/* projetos */}
+      <section className='w-full pb-[120px]'>
+        <div className='w-[calc(100%-40px)] max-w-[1200px] mx-auto'>
+          <h2 className='text-[30px] text-center text-white font-semibold mb-[60px]'>Projetos</h2>
+        </div>
+      </section>
+
+      <Footer />
     </main>
   )
 }
