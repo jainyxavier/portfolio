4ccf8211e0c7e3770b30a6d6e944d2c041505cba
Jainy Xavier <xavierjainy15@gmail.com>
2024-01-27 22:29:50 -0300

    feat: adjustments on responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
       {/* home */}
       <section className='w-full pt-[120px] pb-[120px]'>
-        <div className='w-[calc(100%-40px)] max-w-[1200px] mx-auto flex justify-between items-center gap-[20px] max-[768px]:flex-col'>
+        <div className='w-[calc(100%-40px)] max-w-[1200px] mx-auto flex justify-between items-center gap-[20px] max-[768px]:flex-col max-[768px]:items-start'>
           <div className='w-full max-w-[600px]'>
             <p className='w-fit bg-gradient-to-r from-[#bd3899] via-teal-500 to-fuchsia-500 text-transparent bg-clip-text text-[18px]'>Hello There !</p>
 
@@ -47,7 +47,7 @@ export default function Home() {
           </div>
 
           <Image
-            className='w-full max-w-[400px]'
+            className='w-full max-w-[400px] max-[768px]:ml-auto max-[768px]:max-w-[300px]'
             src="/img/Saly-1.png"
             width={400}
             height={400}
@@ -61,21 +61,21 @@ export default function Home() {
         <div className='w-[calc(100%-40px)] max-w-[1200px] mx-auto'>
           <h2 className='text-[30px] text-center text-white font-semibold mb-[60px]'>About</h2>
 
-          <div className='flex justify-between items-center gap-[40px] max-[768px]:flex-col-reverse'>
+          <div className='flex justify-between items-center gap-[40px] max-[768px]:flex-col-reverse max-[768px]:gap-[80px] max-[768px]:items-start'>
             <Image
-              className='w-full max-w-[350px]'
+              className='w-full max-w-[350px] max-[768px]:max-w-[280px] max-[768px]:ml-auto'
               src="/img/about.png"
               width={350}
               height={350}
               alt="Desenho de pessoas com dúvidas"
             />
 
-            <div className='w-full max-w-[550px]'>
+            <div className='w-full max-w-[550px] max-[768px]:max-w-none'>
               <p className='text-[18px] text-[#99A2A5] font-medium'>
                 Durante os últimos quatro anos, tenho explorado profundamente o vasto universo da tecnologia, concentrando-me no desenvolvimento frontend desde 2020. Em setembro de 2022, tive a oportunidade de aplicar meu conhecimento ao ingressar no mercado de trabalho, colaborando em projetos de outsourcing com uma equipe incrível. Neste momento, estou em busca de novas oportunidades para continuar minha evolução nessa área que me apaixona, ansiosa por adquirir e compartilhar ainda mais conhecimento.
               </p>
 
-              <div className='w-[400px] h-[2px] bg-[#99A2A5] my-[25px]'></div>
+              <div className='w-[80%] max-w-[400px] h-[2px] bg-[#99A2A5] my-[25px]'></div>
 
               <div className='flex flex-col gap-[10px]'>
                 <InfoItem Icon={FaLocationDot} link="https://petrolandiaparallax.netlify.app/" text='Petrolândia-PE' />
@@ -84,7 +84,7 @@ export default function Home() {
               </div>
 
               <div className='flex gap-[40px] mt-[25px]'>
-                <a href="#" target="_blank" className='w-[200px] bg-white text-[#D529A6] font-semibold rounded-lg flex items-center justify-center shadow-lg shadow-gray-500 hover:scale-105'>
+                <a href="/cv-jainy.pdf" target="_blank" className='w-[200px] bg-white text-[#D529A6] font-semibold rounded-lg flex items-center justify-center shadow-lg shadow-gray-500 hover:scale-105'>
                   Visualizar CV
                 </a>
 
@@ -106,7 +106,7 @@ export default function Home() {
         <div className='w-[calc(100%-40px)] max-w-[1200px] mx-auto'>
           <h2 className='text-[30px] text-center text-white font-semibold mb-[60px]'>Skills</h2>
 
-          <ul className='w-full flex items-center justify-between gap-[20px]'>
+          <ul className='w-full flex flex-wrap items-center justify-center gap-[40px]'>
             <ListItemIcon Icon={FaHtml5} size='50px' />
             <ListItemIcon Icon={FaCss3Alt} size='50px' />
             <ListItemIcon Icon={IoLogoJavascript} size='50px' />
@@ -119,11 +119,11 @@ export default function Home() {
       </section>
 
       {/* projetos */}
-      <section className='w-full pb-[120px]'>
+      {/* <section className='w-full pb-[120px]'>
         <div className='w-[calc(100%-40px)] max-w-[1200px] mx-auto'>
-          <h2 className='text-[30px] text-center text-white font-semibold mb-[60px]'>Projetos</h2>
+          <h2 className='text-[30px] text-center text-white font-semibold mb-[60px]'>Projects</h2>
         </div>
-      </section>
+      </section> */}
 
       <Footer />
     </main>
