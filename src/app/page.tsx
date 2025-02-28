e3df88924f673c2cb5d59c38aab11a94d3960960
Jainy Xavier <xavierjainy15@gmail.com>
2025-02-28 15:48:04 -0300

    feat: settings and experience section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { FaGithub, FaLinkedin, FaInstagramSquare, FaPhoneAlt, FaHtml5, FaCss3Alt
 import { FaLocationDot } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 import { IoLogoJavascript } from "react-icons/io5";
-import { SiTailwindcss } from "react-icons/si";
+import { SiTailwindcss, SiTypescript } from "react-icons/si";
 import ListItemIcon from './components/ListItemIcon';
 import InfoItem from './components/InfoItem';
 import DarkMode from './components/DarkMode'
@@ -24,7 +24,7 @@ export default function Home() {
 
       <CursorFollower selectedTheme={selectedTheme} />
       
-      <section className=' w-[calc(100%-120px)] pt-[80px] pb-[120px] flex justify-between gap-[40px] mx-auto'>
+      <section className=' w-[calc(100%-120px)] max-[1024px]:flex-col pt-[80px] pb-[120px] flex justify-between gap-[40px] mx-auto'>
         <div className='w-full max-[768px]:flex-col max-[768px]:items-start'>
           <span className='text-white text-[50px]'>Jaíny Xavier</span>
 
@@ -33,7 +33,7 @@ export default function Home() {
           </h1>
 
           <div className='flex gap-[20px] mt-[25px]'>
-            <a href="/cv-jainy.pdf" target="_blank" className={`${'w-[200px] bg-white text-[#D529A6] font-semibold rounded-lg flex items-center justify-center shadow-lg hover:scale-105'} ${selectedTheme == 'dark' ? 'shadow-gray-500' : 'shadow-[#8c61a3]'}`}>
+            <a href="/cv-jainy-xavier.pdf" target="_blank" className={`${'w-[200px] bg-white text-[#D529A6] font-semibold rounded-lg flex items-center justify-center shadow-lg hover:scale-105'} ${selectedTheme == 'dark' ? 'shadow-gray-500' : 'shadow-[#8c61a3]'}`}>
               Visualizar CV
             </a>
 
@@ -73,10 +73,10 @@ export default function Home() {
         <div className='w-full max-w-[600px]'>
           <h2 className='text-[30px] text-white font-semibold mb-[30px]'>About me</h2>
 
-          <div className='flex justify-between items-center gap-[40px] max-[768px]:flex-col-reverse max-[768px]:gap-[80px] max-[768px]:items-start'>
-            <div className='w-full max-w-[550px] max-[768px]:max-w-none'>
+          <div className='flex justify-between items-center gap-[40px]'>
+            <div className='w-full max-w-[550px]'>
               <p className='text-[18px] text-white font-medium'>
-                Tenho explorado o mundo da tecnologia e me encantado cada vez mais pelo front-end. Escolhi dedicar-me nesta área diariamente para contribuir no mercado da melhor maneira possível. Atualmente, estou em busca de oportunidades que me permitam não só colocar em prática todo o aprendizado adquirido, mas também evoluir continuamente e contribuir ativamente para o crescimento mútuo.
+                Tenho explorado o universo da tecnologia e, a cada dia, me encanto mais pelo desenvolvimento front-end. Decidi dedicar-me integralmente a essa área, buscando aprimorar minhas habilidades e contribuir de forma significativa para o mercado. Atualmente, estou em busca de oportunidades que me permitam aplicar meu conhecimento, evoluir constantemente e colaborar ativamente para o crescimento de projetos e equipes. Além disso, iniciei minha graduação em Sistemas de Informação, reforçando meu compromisso com o aprendizado contínuo e a excelência profissional.
               </p>
 
               <div className='w-[80%] max-w-[400px] h-[2px] bg-white my-[25px]'></div>
@@ -93,19 +93,52 @@ export default function Home() {
 
       <section className='w-full pb-[120px]'>
         <div className='w-[calc(100%-40px)] max-w-[1200px] mx-auto'>
-          <h2 className='text-[30px] text-center text-white font-semibold mb-[60px]'>Skills</h2>
+          <h2 className='text-[30px] text-white font-semibold mb-[30px]'>Experience</h2>
 
-          <ul className='w-full flex flex-wrap items-center justify-center gap-[40px]'>
+          <div className='bg-white/10 hover:bg-white/20 rounded-xl flex gap-[40px] p-[20px]'>
+            <span className='w-[150px] text-white text-[18px] font-medium'>2022 - 2025</span>
+
+            <div className='w-full'>
+              <h3 className='mb-[20px] text-white text-[18px] font-medium'>Frontend Developer - HiveGroup</h3>
+
+              <p className='text-white text-[16px]'>
+                Na HiveGroup, apliquei meu conhecimento na criação e
+                adaptação de aplicações web altamente funcionais e
+                responsivas. Utilizei habilidades em HTML, CSS/SCSS,
+                JavaScript, TypeScript, React.js, Next.js e Tailwind para
+                desenvolver interfaces dinâmicas, atraentes e acessíveis para
+                os usuários. Além disso, fui responsável por integrar essas
+                aplicações com API's garantindo uma experiência fluida e
+                consistente em todos os dispositivos.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='w-full pb-[120px]'>
+        <div className='w-[calc(100%-40px)] max-w-[1200px] mx-auto'>
+          <h2 className='text-[30px] text-center text-white font-semibold mb-[30px]'>Skills</h2>
+
+          <ul className='w-full flex flex-wrap items-center justify-center gap-[40px] max-[1024px]:gap-[20px]'>
             <ListItemIcon Icon={FaHtml5} size='50px' />
             <ListItemIcon Icon={FaCss3Alt} size='50px' />
-            <ListItemIcon Icon={IoLogoJavascript} size='50px' />
             <ListItemIcon Icon={FaSass} size='50px' />
             <ListItemIcon Icon={SiTailwindcss} size='50px' />
+            <ListItemIcon Icon={IoLogoJavascript} size='50px' />
+            <ListItemIcon Icon={SiTypescript } size='50px' />
             <ListItemIcon Icon={FaReact} size='50px' />
-            <ListItemIcon Icon={FaWordpress} size='50px' />
           </ul>
         </div>
       </section>
+
+      {/* <section className='w-full pb-[120px]'>
+        <div className='w-[calc(100%-40px)] max-w-[1200px] mx-auto'>
+          <h2 className='text-[30px] text-center text-white font-semibold mb-[60px]'>Personal Projects</h2>
+
+          Em andamento...
+        </div>
+      </section> */}
 
       <DarkMode changeTheme={(value: string) => setSelectedTheme(value)} />
     </main>
